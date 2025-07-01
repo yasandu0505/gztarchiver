f10ef50c7307df17035fb8de0afa7db26b26fb59
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-01 18:48:51 +0530

    feat : writing function to find the gazette pdfs (local url)

--- gazette_tracker/processor/gazette_processor.py
+++ gazette_tracker/processor/gazette_processor.py
@@ -1,8 +1,20 @@
-def hello():
-    return "Hi i am yasanud imanjith"
+import os
+
+def find_gazette_pdf(base_url):
+    """
+    Recursively walks the given base path and finds all gazette_id_english.pdf files.
+    Returns a list of full file paths.
+    """
     
-if __name__ == "__main__":
-    print("The script is running directly - gazette_processor.py")
-    print(hello())
+    gazette_file = []
+    
+    for dirpath, dirnames, filenames in os.walk(base_url):
+        for filename in filenames:
+            if filename.endswith("_english.pdf"):
+                full_path = os.path.join(dirpath, filename)
+                gazette_file.append(full_path)
+                
+    return gazette_file
+    
 
     
