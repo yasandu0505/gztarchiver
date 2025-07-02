6e7014758824ed43e471bee185db6c6e55f1dfbd
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-02 15:11:47 +0530

    feat : handling the implemented functions from the main

--- dprocessor.py
+++ dprocessor.py
@@ -1,13 +1,24 @@
 import os
-from gazette_tracker.processor import find_gazette_pdf
+from gazette_tracker.processor import find_gazette_pdf, extract_year_and_id, get_meta_data
+
 
 BASE_DIR = os.path.expanduser("~/Desktop/gazette-archive")
 
 def main():
     files = find_gazette_pdf(BASE_DIR)
-    print(f"Found {len(files)} gazettes....")
-    for path in files[:5]:
-        print("->>>", path )
+    print("\n")
+    print(len(files), "entries found ...\n")
+    for path in files[:50]:
+        meta = extract_year_and_id(path, BASE_DIR)
+        if meta:
+            meta_data = get_meta_data(meta["year"], meta["gazette_id"], BASE_DIR)
+            if meta_data:
+                meta.update(meta_data)
+            
+            print(meta)
+            print("\n")
+            
+            
 
 if __name__ == "__main__":
     main()
