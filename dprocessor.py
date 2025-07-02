089171c2c92ee935a17140488419f979c58a052e
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-02 15:24:32 +0530

    feat : filtering to get only the wanted coloumns for the meta

--- dprocessor.py
+++ dprocessor.py
@@ -8,14 +8,16 @@ def main():
     files = find_gazette_pdf(BASE_DIR)
     print("\n")
     print(len(files), "entries found ...\n")
-    for path in files[:50]:
+    for path in files[:5]:
         meta = extract_year_and_id(path, BASE_DIR)
         if meta:
             meta_data = get_meta_data(meta["year"], meta["gazette_id"], BASE_DIR)
             if meta_data:
-                meta.update(meta_data)
+                wanted_fields = ["gazette_id", "date" , "description"]
+                filtered_meta = {key: meta_data[key] for key in wanted_fields if key in meta_data}
+                meta.update(filtered_meta)
             
-            print(meta)
+            print(filtered_meta)
             print("\n")
             
             
