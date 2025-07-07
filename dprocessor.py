41f3af88b79a16cf0971457ed06a26079f827329
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-07 10:09:01 +0530

    feat : importing modules and use the function to classify

--- dprocessor.py
+++ dprocessor.py
@@ -1,5 +1,5 @@
 import os
-from gazette_tracker.processor import find_gazette_pdf, extract_year_and_id, get_meta_data
+from gazette_tracker.processor import find_gazette_pdf, extract_year_and_id, get_meta_data, classify_gazette
 
 
 BASE_DIR = os.path.expanduser("~/Desktop/gazette-archive")
@@ -8,19 +8,22 @@ def main():
     files = find_gazette_pdf(BASE_DIR)
     print("\n")
     print(len(files), "entries found ...\n")
-    for path in files[:5]:
+    for path in files:
         meta = extract_year_and_id(path, BASE_DIR)
         if meta:
             meta_data = get_meta_data(meta["year"], meta["gazette_id"], BASE_DIR)
             if meta_data:
                 wanted_fields = ["gazette_id", "date" , "description"]
                 filtered_meta = {key: meta_data[key] for key in wanted_fields if key in meta_data}
+                
+                # classify based on the description
+                description = filtered_meta.get("description", "")
+                category = classify_gazette(description)
+                filtered_meta["category"] = category
                 meta.update(filtered_meta)
-            
-            print(filtered_meta)
-            print("\n")
-            
-            
+                print(filtered_meta)
+                print("\n")
+                
 
 if __name__ == "__main__":
     main()
