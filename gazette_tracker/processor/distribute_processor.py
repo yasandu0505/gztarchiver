5ecda0e2134af8e48b544777d40aea2fc7338728
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-07 10:56:01 +0530

    remove : delete the distribute_processor file

--- gazette_tracker/processor/distribute_processor.py
+++
@@ -1,9 +0,0 @@
-def add_file():
-    print("Adding the file ....")
-    print("Done file")
-    return
-
-if __name__ == "__main__":
-    print("Script is running directly - distribute_processor.py")
-    add_file()
-    
