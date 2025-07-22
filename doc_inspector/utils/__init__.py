8492841c433941e8466ea92676a55698165d1f5f
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-22 19:59:21 +0530

    feat : initialize modules for classify docs

--- doc_inspector/utils/__init__.py
+++ doc_inspector/utils/__init__.py
@@ -1,6 +1,8 @@
 from .content_preprocessing_utils import extract_text_from_pdf, prepare_for_llm_processing
+from .categorizing_utils import classify_gazette
 
 __all__ = [
     "extract_text_from_pdf",
-    "prepare_for_llm_processing"
+    "prepare_for_llm_processing",
+    "classify_gazette"
 ]
