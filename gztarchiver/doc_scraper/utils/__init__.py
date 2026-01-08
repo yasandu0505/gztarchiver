6d852c94fb0645022b47f53b1db8775b5188b7a2
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-01-08 18:52:49 +0530

    remove : remove unwanted code files and clean the code base

--- gztarchiver/doc_scraper/utils/__init__.py
+++ gztarchiver/doc_scraper/utils/__init__.py
@@ -2,9 +2,7 @@ from .year_data_utils import load_years_metadata, get_year_link
 from .hide_logs_utils import hide_logs
 from .doc_metadata_utils import filter_doc_metadata, load_doc_metadata_file
 from .archive_folder_utils import create_folder_structure
-from .archive_to_cloud_utils import create_folder_structure_on_cloud, upload_local_documents_to_gdrive, filter_pdf_only, save_upload_results
-from .cloud_credential_utils import get_cloud_credentials
-from .db_utils import prepare_metadata_for_db, connect_to_db, insert_docs_by_year
+from .db_utils import prepare_metadata_for_storing, connect_to_db, insert_docs_by_year
 
 __all__ = [
     "scrape_years_metadata",
@@ -14,12 +12,9 @@ __all__ = [
     "filter_doc_metadata",
     "load_doc_metadata_file",
     "create_folder_structure",
-    "create_folder_structure_on_cloud",
-    "upload_local_documents_to_gdrive",
     "filter_pdf_only",
     "save_upload_results",
-    "get_cloud_credentials",
-    "prepare_metadata_for_db",
+    "prepare_metadata_for_storing",
     "connect_to_db",
     "insert_docs_by_year",
 ]
